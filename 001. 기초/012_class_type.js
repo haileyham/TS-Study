e204f44b2c274144e4bc4cb674dec6741686aa6e
hailey ham <roserealslow@gmail.com>
2023-07-23 11:43:44 +0900

    class type ts

--- 001. 기초/012_class_type.js
+++ 001. 기초/012_class_type.js
@@ -55,3 +55,36 @@ var Person6 = /** @class */ (function () {
 // Person6.prototype.함수다 = function(){} //여기에 하는 것이 아니라 위에 constructor 밑에다가 추가하면 됨
 var 사람4 = new Person6("toto");
 사람4.함수("반가워 토토"); // 사람4에서 Person6의 프로토타입 출력 가능
+//------------------------------------------------------------//
+//------------------------------------------------------------//
+// [1]
+/*
+[Car 클래스를 만들기]
+1. 대충 { model : '소나타', price : 3000 } 이렇게 생긴 object를 복사해주는 class를 만들어보십시오.
+2. 그리고 복사된 object 자료들은 .tax() 라는 함수를 사용가능한데 현재 object에 저장된 price의 10분의1을 출력해주어야합니다.
+3. model과 price 속성의 타입지정도 알아서 잘 해보십시오. tax() 함수의 return 타입도요.
+
+(동작 예시)
+```
+let car1 = new Car('소나타', 3000)
+console.log(car1) //콘솔창 출력결과는 { model : '소나타', price : 3000 }
+console.log(car1.tax()) //콘솔창 출력결과는 300
+```
+*/
+//------------------------------------------------------------//
+// [2]
+/*
+[class인데 파라미터가 잔뜩 들어가는 class Word를 만들기]
+1. object 만들 때 new Word() 소괄호 안에 숫자 혹은 문자를 입력하면
+2. 숫자는 전부 object 안의  num 속성 안에 array 형태로 저장되고
+3. 문자는 전부 object 안의 str 속성 안에 array 형태로 저장되는 class를 만들어봅시다.
+4. class 만들 때 넣을 수 있는 숫자와 문자 갯수는 일단 제한은 없습니다. 그리고 타입 빼먹지 마셈
+
+
+(동작 예시)
+```
+let obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num) //[3,5]
+console.log(obj.str) //['kim', 'park']
+```
+*/ 
