2237332234511d003982e60928e8db6aeb822ab8
hailey ham <roserealslow@gmail.com>
2023-07-26 15:18:12 +0900

    class public

--- 002. TS/004_public,private.js
+++ 002. TS/004_public,private.js
@@ -1,3 +1,17 @@
 //------------------------------------------------------------//
+// class에서 사용하는 
 // public 키워드
 //------------------------------------------------------------//
+var User = /** @class */ (function () {
+    // 사실 public 사용안하면 숨겨져 있다고 생각하면 됨, public 사용안해도 public 부여되어있음
+    function User(a) {
+        this.name = a;
+    }
+    User.prototype.함수 = function () {
+    };
+    return User;
+}());
+var 유저1 = new User('ham');
+console.log(유저1.name); //ham
+유저1.name = "안녕";
+console.log(유저1.name); //안녕
--- 002. TS/004_public,private.ts
+++ 002. TS/004_public,private.ts
@@ -1,4 +1,23 @@
 //------------------------------------------------------------//
+// class에서 사용하는 
 // public 키워드
 //------------------------------------------------------------//
+
+class User{
+    public name: string; //필드값 왼쪽에 public 키워드 사용 : 모든 자식들이 이용가능
+    // 사실 public 사용안하면 숨겨져 있다고 생각하면 됨, public 사용안해도 public 부여되어있음
+    constructor(a:string) {
+        this.name = a;
+    }
+
+    public 함수() {//함수에도 public 키워드 사용 가능
+        
+    }
+}
+
+let 유저1 = new User('ham');
+console.log(유저1.name); //ham
+
+유저1.name = "안녕";
+console.log(유저1.name); //안녕
 
