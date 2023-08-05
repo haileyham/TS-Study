505f2dbdcf38666f804743f54f6a3466101afa3b
hailey ham <roserealslow@gmail.com>
2023-08-05 13:17:52 +0900

    declare, ambient module

---
+++ 002. TS/011_declare&ambient_module.js
@@ -0,0 +1,8 @@
+"use strict";
+//------------------------------------------------------------//
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(데이터자스파일에서정의 + 1); // 10001
+//하고 다른 ts파일에서 사용하면 됨
+var abcd = 'ham';
+console.log(abcd);
+// 크흐...잘 모르겠따..에러가 나고 안나고 ㅠㅠ
