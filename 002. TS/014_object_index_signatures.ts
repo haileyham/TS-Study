cb79eaa4791885f2a15594b02f2c1b41525dc0f8
hailey ham <roserealslow@gmail.com>
2023-08-09 01:27:48 +0900

    signatures

--- 002. TS/014_object_index_signatures.ts
+++ 002. TS/014_object_index_signatures.ts
@@ -115,6 +115,20 @@ let obj = {
 한번에 지정하기 위해 index signature 써보기
 */
 
+type CCar100 = {
+    [key: string]: string | number;
+}
+
+let objj: CCar100 = {
+    model : 'k5',
+    brand : 'kia',
+    price : 6000,
+    year : 2030,
+    date : '6월',
+    percent : '5%',
+    dealer : '김차장',
+}
+
 //------------------------------------------------------------//
 
 //[2]
@@ -134,3 +148,17 @@ let obj = {
 object 안에 object 안에 object가 들어있음.
 타입지정 해보도록 하자. 물론 비슷한 object들이 더 중첩되어도 가능하게 recursive한 타입을 써보자. 
 */
+
+interface CCar101{
+    [key:string] : number | CCar101
+}
+
+let objj2 : CCar101 = {
+    'font-size' : 10,
+    'secondary' : {
+    'font-size' : 12,
+    'third' : {
+    'font-size' : 14
+    }
+}
+}
